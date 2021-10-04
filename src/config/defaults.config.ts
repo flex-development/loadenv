87fd2467c9d672c54d54a9487c28e4e371031b1a
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-04 03:06:09 -0400

    feat: add default options

--- src/config/defaults.config.ts
+++ src/config/defaults.config.ts
@@ -1,6 +1,17 @@
+import type { LoadEnvDefaults } from '@loadenv/types'
+
 /**
  * @file Configuration - Default Options
  * @module loadenv/config/defaults
  */
 
-export default {}
+export default {
+  cascade: undefined,
+  defaults: false,
+  env: '.env',
+  github: false,
+  override: false,
+  print: undefined,
+  root: process.cwd(),
+  verbose: false
+} as LoadEnvDefaults
