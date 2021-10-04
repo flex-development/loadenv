c2fa69152525ca25e3afda7a59e5e3a3f83c60d8
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-10-04 02:42:08 -0400

    feat(typescript): `LoadEnvDefaults`

---
+++ src/types/loadenv-defaults.type.ts
@@ -0,0 +1,39 @@
+import type { LoadEnvOptions } from '@loadenv/interfaces'
+
+/**
+ * @file Type Definitions - LoadEnvDefaults
+ * @module loadenv/types/LoadEnvDefaults
+ */
+
+/**
+ * `loadenv` default options type.
+ * 
+ * @template T - Environment variable names
+ */
+type LoadEnvDefaults = {
+  /** {@link LoadEnvOptions.cascade} */
+  cascade: undefined
+
+  /** {@link LoadEnvOptions.defaults} */
+  defaults: false
+
+  /** {@link LoadEnvOptions.env} */
+  env: NonNullable<LoadEnvOptions['env']>
+
+  /** {@link LoadEnvOptions.github} */
+  github: false
+
+  /** {@link LoadEnvOptions.override} */
+  override: false
+
+  /** {@link LoadEnvOptions.print} */
+  print: undefined
+
+  /** {@link LoadEnvOptions.root} */
+  root: ReturnType<typeof process['cwd']>
+
+  /** {@link LoadEnvOptions.verbose} */
+  verbose: false
+}
+
+export default LoadEnvDefaults
